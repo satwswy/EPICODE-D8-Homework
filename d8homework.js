5fa746e2d67ff628dddce88f170f0d5dd9e3b31b
marios <mgeorgak859@gmail.com>
2022-05-04 15:43:39 +0300

    added exercise 2 solution

--- d8homework.js
+++ d8homework.js
@@ -3,3 +3,14 @@
 let exercise1 = [ 1 , 2 , 3 , 4 , 5]
 console.log(exercise1)
 
+// Exercise 2
+
+let exercise2 = {
+    name : "myname",
+    surname : "mysurname",
+    email : "myemail",
+    age : 30
+}
+console.log(exercise2)
+
+//Exercise 3
