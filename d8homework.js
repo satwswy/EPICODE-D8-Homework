6cbfe15ed4c4d44dfe17401da0697a2d2e9aa8c3
marios <mgeorgak859@gmail.com>
2022-05-04 15:58:22 +0300

    updated exercise 4 solution and added comments

--- d8homework.js
+++ d8homework.js
@@ -1,7 +1,7 @@
 // Exercise 1
 
 let exercise1 = [ 1 , 2 , 3 , 4 , 5]
-console.log(exercise1)
+//console.log(exercise1)
 
 // Exercise 2
 
@@ -11,9 +11,14 @@ let exercise2 = {
     email : "myemail",
     age : 30
 }
-console.log(exercise2)
+//console.log(exercise2)
 
 //Exercise 3
 
 exercise2.hasDrivingLicense = true
-console.log(exercise2)
+//console.log(exercise2)
+
+//Exercise 4
+
+delete exercise2.age 
+//console.log(exercise2)
